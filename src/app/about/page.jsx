8224caee6868df8c1f08487e7996e57cb35b98d6
Marlon042 <127366345+Marlon042@users.noreply.github.com>
2023-04-13 22:48:45 -0600

    2 commit
    carpetas public y src

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+export default function HomePage(){
+    return(
+       
+        <ul style={{color:'white'}}>
+             <h1>
+            DEVELOPED BY:
+            </h1>
+            <li>Yeiko Artavia</li>
+            <li>Carlos Salas</li>
+            <li>Gregory Hidalgo</li>
+            <li>Marlon Gutiérrez V.</li>
+        </ul>
+    )
+}
